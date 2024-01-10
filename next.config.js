40e44163fa5958fc09bfb727ef292a465ca1b7fa
ntn0de <ntn0de@gmail.com>
2024-01-10 21:43:44 +0545

    chore : test deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withMDX = require("@next/mdx")();
 module.exports = withMDX({
-  output: "export",
+  output: 'export',
   pageExtensions: ["js", "jsx", "mdx", "md"],
   experimental: {
     mdxRs: true,
